6c0a685f30455090ba776090cea5ab84d0581f95
silvanoguel <silvanoguel@msn.com>
2023-05-05 17:07:09 +0200

    add:milestone 1 OK

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <!-- **Milestone 1**
-Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Creare un form che invii in GET la lunghezza della password. 
+Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
 Scriviamo tutto (logica e layout) in un unico file *index.php* -->
 
 <?php
 
 // logica 
 
-$length = $_GET['length'];
+$length = intval($_GET['length']);
 
-function password_generate($chars) 
-{
-  $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
-  return substr(str_shuffle($data), 0, $chars);
+function password_generate($chars) {
+    $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+    return substr(str_shuffle($data), 0, $chars);
 }
-  echo password_generate(4);
+echo password_generate($length);
 
 ?>
-
 
 
 <!DOCTYPE html>
@@ -32,7 +31,7 @@ function password_generate($chars)
     <form action="index.php" method="GET">
 
         <label for="word">enter your password length</label>
-        <input type="number" name="length" min="3" max="10" >
+        <input type="number"  name="length" min="3" max="10" >
         
         <button type="submit">Create</button>
         
