28be6ee532a4bc0fba22e71938771c3589eb714a
silvanoguel <silvanoguel@msn.com>
2023-05-05 16:21:42 +0200

    add:milestone 1 to fix

---
+++ index.php
@@ -0,0 +1,43 @@
+<!-- **Milestone 1**
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file *index.php* -->
+
+<?php
+
+// logica 
+
+$length = $_GET['length'];
+
+function password_generate($chars) 
+{
+  $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+  return substr(str_shuffle($data), 0, $chars);
+}
+  echo password_generate(4);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="index.php" method="GET">
+
+        <label for="word">enter your password length</label>
+        <input type="number" name="length" min="3" max="10" >
+        
+        <button type="submit">Create</button>
+        
+    </form>
+
+    
+</body>
+</html>
